aa046f63896a775bc536da861b7e2f7bdcd8eef0
Baturalp Güvenç <45270918+baturalpguvenc@users.noreply.github.com>
2022-10-06 12:32:06 +0300

    Create  degisken_türleri.js

---
+++ degisken_türleri.js
@@ -0,0 +1,61 @@
+// Ekim 1.Perşembe Değişken Türleri -Dante
+//let ve var benzer olarak kullanılabilir değişkenlerde
+
+var urunAdi = "orpheus game"; //string veri
+let urunFiyat = 50; //string (tırnaklar yüzünden)
+
+//matematiksel işlemlerde tırnak kullanılmaz
+
+console.log (typeof urunAdi);
+console.log (typeof urunFiyat);
+
+let sayi1 = "10";
+let sayi2 = "20";
+
+console.log(Number(sayi1) + Number(sayi2));
+//let sayi1 = 10;
+//let sayi2 = 20;
+//console.log(sayi1 + sayi2);
+//yukarıdaki kodlar ile aynı işlemi çalıştırır
+
+//Sayıları Stringe çevirme 
+console.log(sayi1.toString() + sayi2.toString());
+
+let isim = "Dante";
+let soyad = "Alighieri";
+
+console.log(isim + " " + soyad);//ortadaki tırnak aradaki boşluğu koyabilmek için var
+
+let sinavNotu = 70;
+let basarilimi = (sinavNotu >= 50)
+console.log(basarilimi);
+console.log(typeof basarilimi); //boolean
+    let yas;
+console.log(yas);
+console.log(typeof yas);   //içerisine değer atamadığım için undefined olarak belirler türü 
+
+yas == undefined; //değerin bu olduğunu kontrol etmek için 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
